23d26dc84328e1d6b320f87027952616d97e34d4
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2016-11-10 07:58:03 +0100

    Bugfix.

--- build.R
+++ build.R
@@ -1,2 +1,14 @@
-library(devtools)
-build()
+#libs
+library(pacman)
+p_load(devtools, roxygen2)
+
+#make documentation
+try({setwd("./USA.county.data")}, silent = T) #if this fails, it probably means we are already in the right dir
+document()
+
+#install
+setwd("..")
+install("USA.county.data")
+
+#load
+library(USA.county.data)
